36d56ac2bc022dd3e5cd2b1eae446ffb1be9a371
Vitor Franchini Santos - Sistemas de Informação -  Ingresso: 2024 - Integralização: 12/2029 <v284043@ftad.ft.unicamp.br>
2024-10-02 17:31:15 -0300

    added getbyid

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
+const { error } = require("console");
 const express = require("express");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-let task = [];
+let tasks = [];
 let currentID = 1;
 
-app.post("/task",(request, response) => {
+app.post("/tasks",(request, response) => {
     let body = request.body;
     let newTask = {
         id: currentID++,
@@ -16,10 +17,9 @@ app.post("/task",(request, response) => {
         status: body.status,
     };
 
-    task.push(newTask);
+    tasks.push(newTask);
 
     response.status(200).json({message: "Task created successfully", newTask});
-
 
 });
 
@@ -28,9 +28,20 @@ app.get("/oi", (request, response) => {
     return response.status(200).json("Hello World");
 });
 
+app.get("/tasks", (request, response) => {
+    return response.status(200).json(tasks);
+});
 
+app.get("/tasks/:id", (request, response) => {
+    let idRequest = request.params.id;
 
+    const taskFound = tasks.find((task) => task.id == idRequest);
+    if(!taskFound){
+        return response.status(404).json({message: "Task not found"});
+    }
 
+    return response.status(200).json(taskFound);
+});
 
 
 
